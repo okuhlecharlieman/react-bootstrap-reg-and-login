2d91bb7b94e3efc0dc6bfea998a42195a11b663d
Okuhle <okuhlecharlieman72@gmail.com>
2023-09-29 14:51:36 +0200

    a

--- server/index.js
+++ server/index.js
@@ -7,15 +7,27 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(
-  "mongodb+srv://okuhle:okuhle12@okuhle.devxb4e.mongodb.net/Login"
-);
+// Connect to MongoDB
+mongoose
+  .connect("mongodb+srv://okuhle:okuhle12@okuhle.devxb4e.mongodb.net/Login", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
+// Define a POST route to create a user
 app.post("/", (req, res) => {
-  LoginModel.create(req, body).then((user) =>
-    res.json(user).catch((err) => res.json(err))
-  );
+  // You should access the request body using req.body, not req, body
+  LoginModel.create(req.body)
+    .then((user) => res.json(user))
+    .catch((err) => res.status(400).json(err)); // Handle errors properly
 });
+
 app.listen(3001, () => {
-  console.log("server is running");
+  console.log("Server is running on port 3001");
 });
